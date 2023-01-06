402bf895e08e07ed703803ea6d2a763d879c4e7e
ipadorusa <ipadorusa@gmail.com>
2023-01-06 16:38:34 +0900

    진행중

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,6 @@ import GlobalLayout from './pages/_layout'
 const DynamicIndex = React.lazy(() => import('./pages/index'));
 const DynamicProductsIndex = React.lazy(() => import('./pages/products/index'));
 const DynamicProductsId = React.lazy(() => import('./pages/products/[id]'));
-const DynamicQueryClient = React.lazy(() => import('./pages/queryClient'));
 
 
 export const routes = [
@@ -16,7 +15,6 @@ export const routes = [
       { path: '/', element: <DynamicIndex />, index: true},
       { path: '/products', element: <DynamicProductsIndex />, index: true},
       { path: '/products/:id', element: <DynamicProductsId />, },
-      { path: '/queryClient', element: <DynamicQueryClient />, },
     ]
   }
 ]
@@ -25,5 +23,4 @@ export const pages = [
   { route: '/' },
   { route: '/products' },
   { route: '/products/:id' },
-  { route: '/queryClient' },
 ]
